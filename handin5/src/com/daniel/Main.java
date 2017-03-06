9b2407c0997c9f48fc8bd579ffe77dc7b07d73b8
daniel <dgkartin@live.dk>
2017-03-06 20:30:39 +0000

    Last touches

--- handin5/src/com/daniel/Main.java
+++ handin5/src/com/daniel/Main.java
@@ -3,15 +3,15 @@ package com.daniel;
 public class Main {
 
     public static void main(String[] args) {
+        //First tread
         Thread t1 = new Thread(new Stuff(0));
+        //Second thread
         Thread t2 = new Thread(new Stuff(0));
-        Thread t3 = new Thread(new Stuff(0));
-        Thread t4 = new Thread(new Stuff(0));
 
+        //Starting the threads
         t1.start();
         t2.start();
-        t3.start();
-        t4.start();
+
     }
 
 
--- handin5/src/com/daniel/Stuff.java
+++ handin5/src/com/daniel/Stuff.java
@@ -2,14 +2,17 @@ package com.daniel;
 
 public class Stuff implements Runnable{
 
+    //Number variable
     int y;
     public Stuff(int x) {
+        //Simply setting the start value, so it can be customized per thread
         y = x;
     }
 
     @Override
     public void run() {
         try  {
+            //Counting to three
             while(y <= 3) {
                 System.out.println(y);
                 y++;
