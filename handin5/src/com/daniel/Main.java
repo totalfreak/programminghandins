f0ad7f447c66938989fe0f99c99e3fb46bcd1a1d
daniel <dgkartin@live.dk>
2017-03-08 18:50:23 +0000

    Final final touches

--- handin5/src/com/daniel/Main.java
+++ handin5/src/com/daniel/Main.java
@@ -3,15 +3,15 @@ package com.daniel;
 public class Main {
 
     public static void main(String[] args) {
-        //First tread
-        Thread t1 = new Thread(new Stuff(0));
-        //Second thread
-        Thread t2 = new Thread(new Stuff(0));
 
-        //Starting the threads
-        t1.start();
-        t2.start();
+            //First tread
+            Thread t1 = new Thread(new Stuff(1));
+            //Second thread
+            Thread t2 = new Thread(new Stuff(1));
 
+            //Starting the threads
+            t1.start();
+            t2.start();
     }
 
 
